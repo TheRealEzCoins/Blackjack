fb7e6925de2d6e88e5fd79c4e7fb6a895c983c3a
TheRealEzCoins <stevens.ian633@gmail.com>
2022-11-18 13:47:53 +0100

    No more duplicates, - Using Kaarten.cs constructor

---
+++ Blackjack/Kaarten.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+
+    class Kaarten
+    {
+        private string naam;
+        private int nummer;
+        public Kaarten(String naam, int nummer) {
+            this.naam = naam;
+            this.nummer = nummer;
+
+        }
+        
+        public string getNaam()
+        {
+            return naam;
+        }
+
+        public int getNummer() 
+        {
+            return nummer;
+        }
+      
+    }
+}
